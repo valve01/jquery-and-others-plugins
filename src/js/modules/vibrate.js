b7f22837cbe5039b18934c595447ce44e59eaf6a
Vikenty Mytko <118441831+valve01@users.noreply.github.com>
2024-02-26 16:51:31 +0300

    ь

--- src/js/modules/vibrate.js
+++ src/js/modules/vibrate.js
@@ -1,5 +1,3 @@
 import $ from 'jquery';
-import '../modules/jquery/jquery.vibrate.min.js';
-$(".facebook-link").vibrate({
-    pattern: [20, 200, 20]
-});
+import '../modules/jquery/jquery.vibrate';
+$(".footer").vibrate();
