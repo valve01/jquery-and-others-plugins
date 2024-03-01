dbe2325e3a6319f397af1c94783289905552a4b9
Vikenty Mytko <118441831+valve01@users.noreply.github.com>
2024-03-01 15:47:32 +0300

    5

--- src/js/index.js
+++ src/js/index.js
@@ -16,7 +16,7 @@ import './modules/reviews-slider';
 // import './modules/jQueryModules/apexCharts';
 // import './modules/jQueryModules/gio';
 // import './modules/jQueryModules/tui';
-import './modules/jQueryModules/datepicker';
+// import './modules/jQueryModules/datepicker';
 
 
 
